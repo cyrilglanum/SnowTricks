5b298e5aa339bcbb5f1510716dc5062ab21deae8
Cyril <cyril@glanum.com>
2021-11-15 11:48:49 +0100

    ST : Add trick to database with form

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -23,6 +23,36 @@ class TrickController extends AbstractController
         $trick->setName('Hello World');
         $trick->setImgBackground('enbfbsbfbe262511.jpg');
         $trick->setDescription('Ce trick reprend les bases du snowboard ...');
+        $trick->setDateCreation(new \DateTime('now'));
+
+
+        $form = $this->createForm(TrickType::class, $trick);
+
+        $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($trick);
+        $em->flush();
+
+    }
+
+        return $this->render('tricks/newTrick.html.twig', array(
+            'form' => $form->createView(),
+            'message' => "le trick a bien été ajouté"
+        ));
+    }
+
+
+    /**
+     * @Route("/tricks")
+     */
+    public function tricks(Request $request)
+    {
+        $trick = new Tricks();
+        $trick->setName('Hello World');
+        $trick->setImgBackground('enbfbsbfbe262511.jpg');
+        $trick->setDescription('Ce trick reprend les bases du snowboard ...');
 
         $form = $this->createForm(TrickType::class, $trick);
 
