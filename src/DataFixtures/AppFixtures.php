9bcc42a3451804469c11a3b6c8b6f2d29722710f
Cyril <cyril@glanum.com>
2021-11-24 12:44:19 +0100

    ST: fix sur le rendu des dates en twig + add fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,30 @@
+<?php
+
+// src/DataFixtures/AppFixtures.php
+namespace App\DataFixtures;
+
+use App\Entity\Tricks;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        // create 20 products! Bam!
+        for ($i = 0; $i < 3; $i++) {
+            $product = new Tricks();
+            $product->setName('trick '.$i);
+            $product->setName('trick '.$i);
+            $product->setDescription('description');
+            $product->setImgBackground('urld151502020.jpg');
+            $product->setGroupe('tricks');
+//            $product->setDateCreation(Date('Y-m-d H:i:s', ));
+//            $product->setDateModification(\DateTime::createFromFormat('Y-m-d H:i:s'));
+
+            $manager->persist($product);
+        }
+
+        $manager->flush();
+    }
+}
